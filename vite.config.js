00768873b18c4a7dfabf6f4893d246dfddc7dd3e
izereuwonkunda <izereuwonkundakelia@gmail.com>
2023-04-06 12:44:08 +0200

    fix:package-json errors

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import react from '@vitejs/plugin-react';
-import eslint from '@rollup/plugin-eslint';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  plugins: [
-    react(),
-    eslint({
-      include: ['src/**/*.js'],
-      exclude: ['node_modules/**', 'dist/**'],
-      throwOnError: true,
-      throwOnWarning: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      'react/jsx-runtime': require.resolve('react/jsx-runtime'),
-    },
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
