c0f08671153a283d42257c28a4253e76e28904fe
izereuwonkunda <izereuwonkundakelia@gmail.com>
2023-04-17 17:55:42 +0200

    setting up SASS

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import '../scss/login.scss';
+import '../css/login.css'
 export default function Login() {
   return (
     <div>
       <h3>Login Page</h3>
+      <button class="button-primary">Login</button>
     </div>
   );
 }
