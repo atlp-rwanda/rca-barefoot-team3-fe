82d905b4123f8f73ad8e21aece8c4714d5b660e5
Shallon Kobusinge <skobusinge@centrika.rw>
2023-04-21 13:17:40 +0200

    fix: lint errors

--- src/components/SocialButton.js
+++ src/components/SocialButton.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 export default function SocialButton({ icon, text }) {
   return (
-    <div className="my-2 w-full h-14 bg-white border rounded border-primary flex justify-center items-center gap-4" >
+    <div className="my-2 w-full h-14 bg-white border rounded border-primary flex justify-center items-center gap-4">
       {icon}
       <h2 className="text-primary">{text}</h2>
     </div>
