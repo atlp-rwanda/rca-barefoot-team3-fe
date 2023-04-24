0c5d76793c45dcc30f1af9c867e48b20412b238f
tracy8 <tracymurenzi@gmail.com>
2023-04-24 15:10:35 +0200

    ch-created-button-component

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../../scss/login.scss';
+import Button from '../../src/components/buttons/button';
 
 export default function Login() {
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
   return (
     <div>
-      <h3>Login Page</h3>
-      <button type="button" className="button-primary">Login</button>
+      <h1 className="text-4xl">Login page</h1>
+      <Button onClick={handleClick} className="bg-green-500">
+        Login 
+      </Button>
     </div>
   );
 }
