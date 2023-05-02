2b348331da1025060a56a93ad4dc6856257480eb
Shallon Kobusinge <skobusinge@centrika.rw>
2023-05-02 02:08:49 +0200

    add: reset password page

--- src/views/ResetPassword.js
+++ src/views/ResetPassword.js
@@ -1,26 +1,43 @@
-import { Formik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import React, { useEffect } from "react";
 
 export default function ResetPassword() {
   return (
-    <div className="  md:flex justify-center w-full h-screen bg-orange-light ">
-      <div className="flex  p-4 md:w-8/12 md:p-24 flex flex-col justify-center items-center ">
-        <div className="w-8/12 md:w-6/12 h-3/6 bg-white p-4 ">
-          <div>
-            <h1 className="text-3xl font-bold  text-gray-900">
-              Reset Password
+    <div className=" flex justify-center w-full h-screen bg-orange-light ">
+      <h2 className="text-3xl font-bold mt-6 md:mt-12">Barefoot</h2>
+      <div className="md:w-10/12 flex flex-col justify-center items-center ">
+        <div className="w-6/12 bg-white py-4 md:py-16 flex flex-col items-center ">
+          <div className="w-6/12 md:mr-32">
+            <h1 className="text-3xl font-bold text-gray-900">
+              Reset your Password
             </h1>
+            <p className="py-2 text-md text-grey-dark font-light">
+              Enter your email to reset password and regain access to your
+              account
+            </p>
           </div>
-          <Formik>
-            <Form>
-              <Field
-                name="email"
-                component="input"
-                type="text"
-                placeholder="Enter your email address"
-              />
-            </Form>
-          </Formik>
+          <div className=" w-2/3 ">
+            <Formik>
+              <Form>
+                <div className="py-4">
+                  <p className="my-4 text-grey-dark font-light ">Email address</p>
+                  <Field
+                    name="email"
+                    component="input"
+                    type="text"
+                    placeholder="Enter your email address"
+                    className="border border  font-light w-full p-3"
+                  />
+                </div>
+                <button
+                    type="submit"
+                    className="my-2 w-full h-14 button-primary"
+                  >
+                    Submit
+                  </button>
+              </Form>
+            </Formik>
+          </div>
         </div>
       </div>
     </div>
