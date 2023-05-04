72a032ebe9de0b01dce4c1bb8b8203d09de018db
tracy8 <tracymurenzi@gmail.com>
2023-05-04 16:41:38 +0200

    ch-fixed-linters

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -56,7 +56,7 @@ export default function Dashboard() {
               <div className="flex-1 px-3 bg-primary">
                 {links.map(({ name, path }) => (
                   <div className="space-y-2 pt-2">
-                    <button onClick={() => setActiveLink(path)} className="text-base font-normal rounded-lg transition duration-75 flex items-center p-2" rel="noreferrer">
+                    <button onClick={() => setActiveLink(path)} className="text-base font-normal rounded-lg transition duration-75 flex items-center p-2">
                       <svg className="w-6 h-6 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
                         <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
